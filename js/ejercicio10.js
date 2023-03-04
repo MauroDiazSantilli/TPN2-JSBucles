33ba96cdf147d29e8270f7c1e9bf19d9ce4c843a
MauroDS <mauro.diaz.santilli@gmail.com>
2023-03-04 18:02:20 -0300

    Terminando ejercicio 10

--- js/ejercicio10.js
+++ js/ejercicio10.js
@@ -1,1 +1,22 @@
-let i,j
+let i,j;
+let filas = Number(prompt("Introduzca el numero de filas"));
+let column = Number(prompt("Introduzca un numero de columnas"))
+let result = filas * column;
+document.write("<table border>");
+for(i = 0; i < filas; i++){
+    document.write("<tr>")
+
+    for(j = 0; j < column; j++){
+        document.write("<td>");
+
+        document.write(result);
+
+         result--
+
+        document.write("</td>")
+        }
+
+        document.write("</tr>")     
+    }
+    
+    document.write("</table>")
