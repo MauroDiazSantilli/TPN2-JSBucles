434a0ac8a044cd0f6b070e59a54c7fc59577bba6
MauroDS <mauro.diaz.santilli@gmail.com>
2023-03-06 15:24:05 -0300

    Terminando ejercicio 16

---
+++ js/ejercicio16.js
@@ -0,0 +1,12 @@
+let cadena = prompt("Introduzca un texto");
+let numeCarac = cadena.length;
+let j;
+let caracteres;
+let resultado = "";
+
+for(j = 0; j < numeCarac; j++){
+    caracteres = cadena.charAt(j);
+    resultado = caracteres + resultado;
+}
+
+document.write(resultado);
